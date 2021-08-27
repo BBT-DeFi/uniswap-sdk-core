8800e42196f641dc01f8d2d40425770fff1d55e7
Thanakait Homhual <61160061@go.buu.ac.th>
2021-08-27 18:56:29 +0700

    Revert commit: 4c4ed48

--- src/entities/ether.ts
+++ src/entities/ether.ts
@@ -1,30 +1,27 @@
+import invariant from 'tiny-invariant'
 import { Currency } from './currency'
 import { NativeCurrency } from './nativeCurrency'
 import { Token } from './token'
+import { WETH9 } from './weth9'
 
 /**
  * Ether is the main usage of a 'native' currency, i.e. for Ethereum mainnet and all testnets
  */
 export class Ether extends NativeCurrency {
-  private _wrappedEtherAddress: string
-  private _wrappedEther: Token | undefined
-
-  protected constructor(chainId: number, address: string) {
+  protected constructor(chainId: number) {
     super(chainId, 18, 'ETH', 'Ether')
-    this._wrappedEtherAddress = address
   }
 
   public get wrapped(): Token {
-    if (this._wrappedEther) {
-      return this._wrappedEther
-    }
-
-    this._wrappedEther = new Token(this.chainId, this._wrappedEtherAddress, 18, 'WETH', 'Wrapped Ether')
-    return this._wrappedEther
+    const weth9 = WETH9[this.chainId]
+    invariant(!!weth9, 'WRAPPED')
+    return weth9
   }
 
-  public static onChain(chainId: number, address: string): Ether {
-    return new Ether(chainId, address)
+  private static _etherCache: { [chainId: number]: Ether } = {}
+
+  public static onChain(chainId: number): Ether {
+    return this._etherCache[chainId] ?? (this._etherCache[chainId] = new Ether(chainId))
   }
 
   public equals(other: Currency): boolean {
